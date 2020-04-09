658d3196e731ffb7852310f3330349ca6662ff81
Alexander Kozienko <a.s.kozienko@gmail.com>
2020-04-09 11:52:24 +0300

    COR-5227: do not error usercode

--- test/apps/testrunner.go
+++ test/apps/testrunner.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/corezoid/gitcall-go-runner/gitcall"
 )
 
 func usercode(_ context.Context, data map[string]interface{}) error {
-	data["foo"] = 123
+	switch data["case"] {
+	case "success":
+		data["foo"] = 123
+
+		return nil
+	case "error":
+		return fmt.Errorf("error-happened")
+	case "panic":
+		panic("something went wrong")
+	}
 
 	return nil
 }
